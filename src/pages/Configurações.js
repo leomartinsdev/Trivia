66fb24af5481ef036f989057ed27ab0adaa978fc
Matheus Yelgde <yelgde.8@gmail.com>
2023-05-22 18:01:47 -0300

    Requisito 3 completo

---
+++ src/pages/Configurações.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Configurações extends Component {
+  render() {
+    return (
+      <div>
+        <h1 data-testid="settings-title">Configurações</h1>
+      </div>
+    );
+  }
+}
