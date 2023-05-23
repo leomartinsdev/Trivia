d46dd589fe6f73151c02f1adf15ee3f58c16ae74
Matheus Yelgde <yelgde.8@gmail.com>
2023-05-23 19:51:10 -0300

    Requisito 7 completo, só falta finalizar lint

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -6,9 +6,7 @@ export default class Questions extends Component {
     shuffler: true,
     questionsSort: [],
     correct: '',
-    // isToClear: false,
-    // seconds: 30,
-    // isDisabled: false,
+    selectedAnswer: null,
   };
 
   componentDidUpdate() {
@@ -18,9 +16,8 @@ export default class Questions extends Component {
   shufflerCondition = () => {
     const { questions } = this.props;
     const eachQuestion = questions[0];
-    const { shuffler, questionsSort } = this.state;
+    const { shuffler } = this.state;
     const correctAnswer = eachQuestion.correct_answer;
-    console.log(correctAnswer);
     const incorrectAnswers = eachQuestion.incorrect_answers;
     const fisherYates = 0.5;
     if (shuffler) {
@@ -31,44 +28,55 @@ export default class Questions extends Component {
         shuffler: false,
         correct: correctAnswer,
       });
-      return shuffledAnswers;
-    } return questionsSort;
+    }
+  };
+
+  handleAnswerClick = (answer) => {
+    this.setState({ selectedAnswer: answer });
   };
 
   render() {
     const { questions } = this.props;
     if (!questions) {
-      return (<p>Error</p>);
+      return <p>Error</p>;
     }
-    const { questionsSort, correct } = this.state;
-    console.log(questionsSort);
+    const { questionsSort, correct, selectedAnswer } = this.state;
+
     return (
       <div>
-        { questions.length > 0
-          && (
+        {questions.length > 0 && (
+          <div>
+            <p data-testid="question-category">{questions[0].category}</p>
+            <p data-testid="question-text">{questions[0].question}</p>
             <div>
-              <p data-testid="question-category">{questions[0].category}</p>
-              <p data-testid="question-text">{questions[0].question}</p>
-              <div>
-                <div data-testid="answer-options">
-                  {questionsSort.map((answer, index) => (
-                    <button
-                      key={ index }
-                      className="answer-button"
-                      value={ answer }
-                      data-testid={
-                        answer === correct
-                          ? 'correct-answer'
-                          : `wrong-answer-${index}`
-                      }
-                    >
-                      { answer }
-                    </button>
-                  ))}
-                </div>
+              <div data-testid="answer-options">
+                {questionsSort.map((answer, index) => (
+                  <button
+                    onClick={ () => this.handleAnswerClick(answer) }
+                    key={ index }
+                    className="answer-button"
+                    value={ answer }
+                    style={ {
+                      border:
+                        selectedAnswer === null
+                          ? ''
+                          : answer === correct
+                            ? '3px solid rgb(6, 240, 15)'
+                            : '3px solid red',
+                    } }
+                    data-testid={
+                      answer === correct
+                        ? 'correct-answer'
+                        : `wrong-answer-${index}`
+                    }
+                  >
+                    {answer}
+                  </button>
+                ))}
               </div>
             </div>
-          )}
+          </div>
+        )}
       </div>
     );
   }
