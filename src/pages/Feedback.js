ae4eccc726caedaf30828bc6d55379988b31a066
Joatan Feitosa <joatancarlos@gmail.com>
2023-05-25 15:23:50 -0300

    requisito 14 e 15 finalizados

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -6,8 +6,13 @@ import Header from '../components/Header';
 const minAssertions = 3;
 
 class Feedback extends Component {
+  handleClick = () => {
+    const { history } = this.props;
+    history.push('/');
+  };
+
   render() {
-    const { assertions } = this.props;
+    const { assertions, score } = this.props;
     return (
       <div>
         <Header />
@@ -17,7 +22,15 @@ class Feedback extends Component {
               ? 'Well Done!'
               : 'Could be better...'
           }
+          <p data-testid="feedback-total-score">{score}</p>
+          <p data-testid="feedback-total-question">{assertions}</p>
         </p>
+        <button
+          data-testid="btn-play-again"
+          onClick={ this.handleClick }
+        >
+          Play Again
+        </button>
       </div>
     );
   }
