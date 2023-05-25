d719090a9a0233070d839b18f783a88ffdf9d0a8
Anderson Silva <nutriandersonsilva@gmail.com>
2023-05-25 14:41:02 -0300

    atividade 12

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Header from '../components/Header';
 
-export default class Feedback extends Component {
+class Feedback extends Component {
   render() {
     return (
       <div>
+        <Header />
         <p data-testid="feedback-text" />
       </div>
     );
   }
 }
+
+Feedback.propTypes = {
+  score: PropTypes.number,
+}.isRequired;
+
+const mapStateToProps = (state) => ({
+  email: state.player.gravatarEmail,
+  userName: state.player.name,
+  score: state.player.score,
+});
+
+export default connect(mapStateToProps)(Feedback);
